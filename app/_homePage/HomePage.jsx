d1ccd28f22629cc380f84673444ad8fc2886c755
gautamkumar1 <gautamraaz936@gmail.com>
2024-06-20 01:03:33 +0530

    update HomePage.jsx

--- app/_homePage/HomePage.jsx
+++ app/_homePage/HomePage.jsx
@@ -34,6 +34,7 @@ export default function HomePage() {
               </Link>
               <Link
                 href="https://www.linkedin.com/in/gautamkum4r/"
+                target="blank"
                 className="inline-flex h-9 items-center justify-center rounded-md border border-input bg-background dark:bg-muted px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 "
                 prefetch={false}
               > 
